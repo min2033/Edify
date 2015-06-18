21eb9d1c62032a421a4388f4981b426fd8b58f82
Hack Reactor Students <accounts@hackreactor.com>
2015-06-18 10:21:29 -0700

    minor edit to fix package.json and username in mysql setup

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -3,7 +3,7 @@ var knex = require('knex')({
   client: 'mysql',
   connection: {
     host: '127.0.0.1',
-    user: 'spork',
+    user: 'root',
     password: '',
     database: 'spork',
     charset: 'utf8'
