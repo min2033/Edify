405267c1b458a7f434a6ca09556302341b27c34f
mikemsrk <min2033@gmail.com>
2015-06-23 16:21:08 -0700

    Gruntfile Build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,37 @@ module.exports = function(grunt) {
       }
     },
 
+    ngAnnotate: {
+      app: {
+          files: {
+                  'client/dist/annotated.js': [
+                    'client/app/controllers/allskills.js',
+                    'client/app/controllers/user.js',
+                    'client/app/controllers/main.js',
+                    'client/app/controllers/skill.js',
+                    'client/app/services/services.js',
+                    'client/app/app.js'
+                    ]
+                  },
+          }
+    },
+
+    jshint: {
+      files: [
+        // Add filespec list here
+        'client/app/controllers/allskills.js',
+        'client/app/controllers/user.js',
+        'client/app/controllers/main.js',
+        'client/app/controllers/skill.js',
+        'client/app/services/services.js',
+        'client/app/app.js'
+      ],
+      options: {
+        force: 'true',
+        ignores: []
+      }
+    },
+
     shell: {
       multiple: {
         command: [
@@ -27,24 +58,60 @@ module.exports = function(grunt) {
       }
     },
 
+    clean: {
+          js: ["client/dist/*.js","!client/dist/*.min.js"]
+        },
+    concat: {
+      basic: {
+        src: ['client/bower_components/angular/angular.js',
+              'client/bower_components/angular-ui-router/release/angular-ui-router.js',
+              'client/bower_components/jquery/dist/jquery.js',
+              'client/bower_components/bootstrap/dist/js/bootstrap.js',
+              'client/bower_components/angular-bootstrap/ui-bootstrap-tpls.min.js'
+              ],
+        dest: 'client/dist/lib.js' 
+      }
+    },
+
     uglify: {
-      build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+      my_target: {
+        files: {
+          'client/dist/main.min.js': ['client/dist/annotated.js'],
+          'client/dist/lib.min.js': ['client/dist/lib.js']
+        }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: {
+          'client/dist/style.min.css': ['client/bower_components/bootstrap/dist/css/bootstrap.css','client/styles/style.css']
+        }
       }
     }
+
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-mysql-dump');
   grunt.loadNpmTasks('grunt-shell');
-
-
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-ng-annotate');
 
   // Default task(s).
   grunt.registerTask('default', []);
-  grunt.registerTask('backup', ['db_dump']);
-  grunt.registerTask('reset', ['shell']);
+  grunt.registerTask('backup', ['db_dump']);  // Save current MySQL data as pre-fill.
+  grunt.registerTask('reset', ['shell']); // Reset DB with pre-fill data.
+  grunt.registerTask('build', [
+    'jshint',
+    'concat',
+    'uglify',
+    'cssmin',
+    'clean'
+  ]);
 
 };
--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,5 @@
 angular.module('edify',[
   'ui.router',
-  'edify.auth',
   'edify.services',
   'edify.user',
   'edify.allskills',
