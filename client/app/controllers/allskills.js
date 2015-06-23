f5069158928e312c0418215828ed1c723e3843b4
Timmy Luong <tcl.luong@gmail.com>
2015-06-22 18:22:50 -0700

    Update allSkills client view

--- client/app/controllers/allskills.js
+++ client/app/controllers/allskills.js
@@ -1,12 +1,18 @@
 angular.module('edify.allskills', [])
 
 .controller('AllSkillsController', function($scope, Skills) {
-  $scope.allSkills;
+  $scope.allSkills = [];
 
   $scope.getAllSkills = function () {
     Skills.getAllSkills()
     .then(function (skills) {
-      $scope.allSkills = skills;
+      for (var key in skills) {
+        $scope.allSkills.push(key);    
+      }
+    })
+    .catch(function (error) {
+      console.error(error);
     });
   };
+  $scope.getAllSkills();
 });
