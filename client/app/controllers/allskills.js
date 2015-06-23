d3e970a92aa959b09e7c363d842ff4ee3ebb8128
Timmy Luong <tcl.luong@gmail.com>
2015-06-23 16:02:59 -0700

    Add sorting by number of teachers or learners on AllSkills view in client

--- client/app/controllers/allskills.js
+++ client/app/controllers/allskills.js
@@ -5,17 +5,18 @@ angular.module('edify.allskills', [])
   // Use this assignment only for STATIC data testing
   //   - comment out when doing real database testing
   // $scope.allSkills = [
-  //                     ["javascript", {learners: [{},{},{},{}], teachers: [{},{},{},{}], skill_id: 100} ],
-  //                     ["backbone", {learners: [{},{}], teachers: [{}], skill_id: 200} ],
-  //                     ["angular", {learners: [{},{},{}], teachers: [{},{},{}], skill_id: 300} ],
-  //                     ["node", {learners: [{},{},{},{},{}], teachers: [{},{}], skill_id: 400} ],
-  //                     ["databases", {learners: [{},{},{},{},{},{},{}], teachers: [], skill_id: 500} ],
-  //                     ["java", {learners: [{},{},{},{},{}], teachers: [{},{},{},{},{},{},{},{}], skill_id: 666} ],
-  //                     ["data structures", {learners: [{}], teachers: [{},{},{},{},{}], skill_id: 99} ],
+  //                     { skill_name: "javascript", learners: [{},{},{},{}], teachers: [{},{},{},{}], skill_id: 100 },
+  //                     { skill_name: "backbone", learners: [{},{}], teachers: [{}], skill_id: 200 },
+  //                     { skill_name: "angular", learners: [{},{},{}], teachers: [{},{},{}], skill_id: 300 },
+  //                     { skill_name: "node", learners: [{},{},{},{},{}], teachers: [{},{}], skill_id: 400 },
+  //                     { skill_name: "databases", learners: [{},{},{},{},{},{},{}], teachers: [], skill_id: 500 },
+  //                     { skill_name: "java", learners: [{},{},{},{},{}], teachers: [{},{},{},{},{},{},{},{}], skill_id: 666 },
+  //                     { skill_name: "data structures", learners: [{}], teachers: [{},{},{},{},{}], skill_id: 99 },
   //                    ];
 
   // USE this assignment for real database testing
   $scope.allSkills = [];
+  $scope.sort = '-learners';
 
   $scope.getProfile = function () {
     if (Auth.isAuth()) {
@@ -34,10 +35,16 @@ angular.module('edify.allskills', [])
   $scope.getAllSkills = function () {
     Skills.getAllSkills()
     .then(function (skills) {
+      var obj = {};
       for (var key in skills) {
-        // console.log([key, skills[key]]);
-        $scope.allSkills.push([key, skills[key]]);
+        obj['skill_name'] = key;
+        obj['learners'] = skills[key].learners;
+        obj['teachers'] = skills[key].teachers;
+        obj['skill_id'] = skills[key].skill_id;
+        $scope.allSkills.push(obj);
+        obj = {};
       }
+      // console.log('allSkills', $scope.allSkills);
     })
     .catch(function (error) {
       console.error(error);
@@ -46,12 +53,16 @@ angular.module('edify.allskills', [])
 
   $scope.getAllSkills();
 
+  $scope.setSort = function (option) {
+    $scope.sort = option;
+  };
+
   $scope.addLearnSkill = function(index) {
     // debugger;
     var skill = {
       type: 'learn',
       // skill: $scope.allSkills[index].skill_name,
-      skill: $scope.allSkills[index][0],
+      skill: $scope.allSkills[index].skill_name,
       skillLevel: 1,
       userId: $scope.user.id
     };
@@ -64,7 +75,7 @@ angular.module('edify.allskills', [])
     var skill = {
       type: 'teach',
       // skill: $scope.user.teachSkills[index].skill_name,
-      skill: $scope.allSkills[index][0],
+      skill: $scope.allSkills[index].skill_name,
       skillLevel: 1,
       userId: $scope.user.id
     };
