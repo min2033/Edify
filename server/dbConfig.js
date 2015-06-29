617dd85416e62e024b9a9fbe4e15057b4760b5a4
mikemsrk <min2033@gmail.com>
2015-06-28 21:29:13 -0700

    using localhost.

--- server/dbConfig.js
+++ server/dbConfig.js
@@ -2,16 +2,16 @@
 var knex = require('knex')({
   client: 'mysql',
   connection: {
-    // host: '127.0.0.1',
-    // user: 'root',
-    // password: '',
-    // database: 'edify',
-    // charset: 'utf8'
-    host: 'us-cdbr-iron-east-02.cleardb.net',
-    user: 'bb24014bdd49b0',
-    password: 'dc94c0f8',
-    database: 'heroku_94f42c48526e9af',
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'edify',
     charset: 'utf8'
+    // host: 'us-cdbr-iron-east-02.cleardb.net',
+    // user: 'bb24014bdd49b0',
+    // password: 'dc94c0f8',
+    // database: 'heroku_94f42c48526e9af',
+    // charset: 'utf8'
   }
 });
 
